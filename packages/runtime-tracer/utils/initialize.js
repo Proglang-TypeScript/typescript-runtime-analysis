99e69232d04c6c0ac0e4e9fa5a42ee7fb97fd42e
Peter Thiemann <peter.thiemann@gmail.com>
2026-09-19 21:59:16 -0700

    Use deterministic internal trace identifiers

--- packages/runtime-tracer/utils/initialize.js
+++ packages/runtime-tracer/utils/initialize.js
@@ -6,6 +6,11 @@
   sandbox.runTimeInfo = {};
   sandbox.publicExports = {schemaVersion: 1, policy: 'commonjs-own-descriptor-v1', pathsByFunctionId: {}, exclusions: [], matchedRequires: []};
   sandbox.transparentTracing = process.env.TRACE_TRANSPARENT === '1';
+  var nextTraceId = 0;
+  sandbox.newTraceId = function (prefix) {
+    nextTraceId++;
+    return prefix + '-' + nextTraceId;
+  };
   sandbox.observationCount = 0;
   sandbox.recordObservation = function () {
     sandbox.observationCount++;
